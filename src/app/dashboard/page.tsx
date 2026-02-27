e99b01f37001a0476719c2974782a865c4323e32
CorretorBrazza <corretorbrazza@gmail.com>
2026-02-27 11:28:58 -0300

    fix: corrigido import pendente no dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { getImoveis } from '@/app/actions/imovel-actions';
+import { getImoveis } from '@/app/actions/imovel-server-actions';
 import Link from 'next/link';
 import TabelaImoveis from './TabelaImoveis';
 
